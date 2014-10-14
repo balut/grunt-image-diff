d1ae236ef0b249fb2368f8843efc62ce39aaa6d5
Mike Budahn <mike.budahn@gmail.com>
2014-10-13 21:30:06 -0600

    add percent change

--- tasks/image_diff.js
+++ tasks/image_diff.js
@@ -1,5 +1,5 @@
 /*
- * grunt-image-diff-magick
+ * grunt-image-diff
  * https://github.com/eduardo.pacheco/image-diff
  *
  * Copyright (c) 2013
@@ -14,7 +14,7 @@ var sizeof = require('image-size');
 
 module.exports = function(grunt) {
 
-  grunt.registerMultiTask('image_diff', 'Grunt implementation of Perceptual Image Diff with ImageMagick', function() {
+  grunt.registerMultiTask('image_diff', 'Grunt implementation of Image Magick', function() {
     if (process.platform !== "win32") {
       grunt.log.warn('Only valid in windows, sorry :(.');
       return;
@@ -24,8 +24,8 @@ module.exports = function(grunt) {
       orig: '_orig',
       test: '_test',
       diff: '_diff',
+      highlightColor: 'red',
       fuzz: '0.0',
-      highlight: 'red',
     });
 
     var tests = 0;
@@ -38,40 +38,59 @@ module.exports = function(grunt) {
           return;
         }
         if (!grunt.file.exists(test)) {
-          grunt.log.warn('Test file "' + test + '" invalid.');
+          grunt.log.warn('Test file "' + test + '" not found.');
           return;
         }
+				var orig_dimensions = sizeof(orig);
+				var test_dimensions = sizeof(test);
         
         tests++;
 
-        var origDim = sizeof(orig);
-        var testDim = sizeof(test);
+        //check if dimensions are the same between baseline and test images
+				if(orig_dimensions.width != test_dimensions.width || orig_dimensions.height != test_dimensions.height) {
 
-        //ImageMagick cannot compare 2 images with different dimensions
-        if( origDim.height !== testDim.height || origDim.width !== testDim.width ) {
-            var maxWidth = Math.max(origDim.width, testDim.width);
-            var maxHeight = Math.max(origDim.height, testDim.height);
-            var resizeResult;
+					var maxheight = Math.max(orig_dimensions.height, test_dimensions.height);
+					var maxwidth = Math.max(orig_dimensions.width, test_dimensions.width);
+					var mog_cmd = 'mogrify -extent ' + (maxwidth) + 'x' + (maxheight) + ' -gravity north -fill white ' + orig
+					var result1 = shell.exec(mog_cmd, {silent:true, async:false});
+					mog_cmd = 'mogrify -extent ' + (maxwidth) + 'x' + (maxheight) + ' -gravity north -fill white ' + test
+					result1 = shell.exec(mog_cmd, {silent:true, async:false});
+				}
+			
+        //user prefs  
+				var cmdOptions = '';
+				if (options.fuzz) {
+					cmdOptions += '-fuzz "'+ options.fuzz +'%" ';
+				}
+				if (options.highlightColor) {
+					cmdOptions += '-highlight-color '+ options.highlightColor +' ';
+				}
+		
+        //create high contrast diff - this feels like a hack to have to run the comparison twice, but this output is ugly and the other output doesn't show % diff
+				var cmd = 'compare -metric ae -fuzz "'+ options.fuzz +'%" -highlight-color white -lowlight-color black -compose src "' + orig + '" "' + test + '" "' + diff + '"'
+        var triggerFail = shell.exec(cmd, {silent:true, async:false});
+				//process output
+        cmd = 'convert "' + diff + '" -format "%[fx:mean]" info:'
+        var percentDiff = shell.exec(cmd, {silent:true, async:false});
+				//create user diff
+        cmd = 'compare -metric ae ' + cmdOptions + '"' + orig + '" "' + test + '" "' + diff + '"'
+        triggerFail = shell.exec(cmd, {silent:true, async:false});
 
-            resizeResult = shell.exec('mogrify -background white -gravity north -extent ' + maxWidth + 'x' + maxHeight + ' "' + orig + '"', {silent: true, async: false});
-            resizeResult = shell.exec('mogrify -background white -gravity north -extent ' + maxWidth + 'x' + maxHeight + ' "' + test + '"', {silent: true, async: false});
-        }
+				//console.log(result2);
+			
+				var fail = triggerFail.code;
 
-        var cmdOptions = '-highlight-color ' + options.highlight + ' ';
-		if (options.fuzz) {
-			cmdOptions += '-fuzz '+ options.fuzz +' ';
-		}
-        var cmd = 'compare -metric rmse ' + cmdOptions + '"' + orig + '" "' + test + '" "' + diff + '" ';
-        var result = shell.exec(cmd, {silent:true, async:false}).output;
-        
-        var fail = result.code;
-
-        if ( fail ) {
-          grunt.fail.warn('FAIL: " diff file created at "' + diff + '"');
-        }
+        //output
+				if ( fail ) {
+					grunt.log.warn('FAIL: ' + Math.round(percentDiff.output * 100) + '% difference found - see diff file ' + diff);
+				} else {
+      		grunt.log.ok('PASS: Diff file "' + diff + '" created');					
+				}
+				
       });
     });
 
+		console.log('\n');
     if (tests > 0) {
       grunt.log.ok(tests + " tested image(s).");
     } else {
